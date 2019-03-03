62132f67c7a1b3ddbe54e45612eaa53d1d137c06
Johan Kristell <johan@jott.se>
2019-03-03 16:06:48 +0100

    Use the new gpio interrupt api.

--- examples/button-interrupt.rs
+++ examples/button-interrupt.rs
@@ -10,18 +10,19 @@ use core::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
 
 use cortex_m_rt::entry;
 
+use board::gpio::{Edge, ExtiPin};
 use board::hal::prelude::*;
 use board::hal::{interrupt, stm32};
 use board::Interrupt;
 
 use cortex_m::peripheral::Peripherals;
 
-static LED_STATE: AtomicBool = ATOMIC_BOOL_INIT;
+static SIGNAL: AtomicBool = ATOMIC_BOOL_INIT;
 
 #[entry]
 fn main() -> ! {
     // The Stm32 peripherals
-    let device = stm32::Peripherals::take().unwrap();
+    let mut device = stm32::Peripherals::take().unwrap();
     // The Cortex-m peripherals
     let mut core = Peripherals::take().unwrap();
 
@@ -31,7 +32,7 @@ fn main() -> ! {
 
     // Configure PC5 (User B1) as an input
     let gpioc = device.GPIOC.split();
-    let _button = gpioc.pc13.into_pull_up_input();
+    let mut button = gpioc.pc13.into_pull_up_input();
 
     // Enable the clock for the SYSCFG
     device.RCC.apb2enr.modify(|_, w| w.syscfgen().enabled());
@@ -40,26 +41,19 @@ fn main() -> ! {
     let rcc = device.RCC.constrain();
     let _clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
 
-    // Enable interrupt on PC13
-    device.SYSCFG.exticr4.write(|w| unsafe { w.exti13().bits(0b0010) });
-
-    // Enable the interrupt on line 13
-    device.EXTI.imr.modify(|_, w| w.mr13().set_bit());
-    // Rising edge
-    device.EXTI.rtsr.modify(|_, w| w.tr13().set_bit());
-    // Falling edge
-    //d.EXTI.ftsr.modify(|_, w| w.tr13().set_bit());
+    // Enable external interrupt on PC13
+    button.make_interrupt_source(&mut device.SYSCFG);
+    button.enable_interrupt(&mut device.EXTI);
+    button.trigger_on_edge(&mut device.EXTI, Edge::RISING);
 
     // Enable the external interrupt
     core.NVIC.enable(Interrupt::EXTI15_10);
 
     loop {
-        //let led_on = unsafe {LED_ON};
-        let led_on = LED_STATE.load(Ordering::Relaxed);
-        if led_on {
-            led.set_high();
-        } else {
-            led.set_low();
+        let state_change = SIGNAL.load(Ordering::Relaxed);
+        if state_change {
+            led.toggle();
+            SIGNAL.store(false, Ordering::Relaxed);
         }
     }
 }
@@ -71,6 +65,6 @@ fn EXTI15_10() {
     unsafe {
         (*stm32::EXTI::ptr()).pr.modify(|_, w| { w.pr13().set_bit() });
     }
-    // Flip the bool
-    LED_STATE.fetch_xor(true, Ordering::SeqCst);
+    // Signal to the man loop that it should toggle the led.
+    SIGNAL.store(true, Ordering::Relaxed);
 }
